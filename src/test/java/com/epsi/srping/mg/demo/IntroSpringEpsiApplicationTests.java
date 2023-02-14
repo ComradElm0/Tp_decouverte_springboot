00b99ccf00d35081267b6e166f3269ef203073e0
ecorb <e.corbeel@yahoo.fr>
2023-02-14 11:12:03 +0100

    2e parties, Saisie données

---
+++ src/test/java/com/epsi/srping/mg/demo/IntroSpringEpsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.srping.mg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringEpsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
